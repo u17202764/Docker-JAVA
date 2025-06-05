bb28de3930fb180ed1328e13762b319a94055a61
Alberto <alberto.limas.contractor@bbva.com>
2025-06-05 10:04:46 -0500

    f

--- src/main/java/com/example/demodockerfile/config_app/WebCorsConfig.java
+++ src/main/java/com/example/demodockerfile/config_app/WebCorsConfig.java
@@ -1,23 +1,22 @@
 package com.example.demodockerfile.config_app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class WebCorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
+        log.info("Configurando CORS para todos los orígenes (sin credenciales)");
+
+        registry.addMapping("/**") // todas las rutas
+                .allowedOrigins("*") // todos los orígenes
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-               // .allowCredentials(true)
-                .maxAge(3600);
+                .maxAge(3600); // cache de preflight request
     }
-
-
-
-
 }
